62f3c6a4ab3a5de1a575a39fa344ae727483b6e8
Lachlan <lachlan.gray2000@gmail.com>
2024-10-02 14:55:53 -0700

    setup py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lloam",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "openai>=1.51.0"
+    ],
+    author="Lachlan Gray",
+    description="A fertile collection of primitives for building things with LLMs",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
