efacbaac015e81fb64226eb470d8c805b371c627
Lachlan <lachlan.gray2000@gmail.com>
2024-12-03 21:20:25 -0500

    next version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lloam",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "openai>=1.51.0"
