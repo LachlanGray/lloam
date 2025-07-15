7a1f0cf4f7b2fe6b970a8a3d87e83e3f8233f68c
Lachlan <lachlan.gray2000@gmail.com>
2025-07-15 13:16:49 -0400

    completion usage example

---
+++ examples/completion_usage.py
@@ -0,0 +1,36 @@
+from lloam.completions import Completion
+
+prompt = "If you were a fruit what would you be?"
+
+
+
+print("# normal usage ##########")
+response = Completion(prompt)
+response.start()
+
+print(response.result())
+
+
+
+print("# stream ##########")
+response = Completion(prompt)
+response.start()
+
+for c in response.stream():
+    print(c, end="")
+print("\n\n")
+
+
+
+print("# run completions in parallel ##########")
+responses = [Completion(prompt) for _ in range(3)]
+
+for r in responses:
+    r.start()
+
+# ... code here would run immediately ...
+
+for r in responses:
+    print(r, end="\n\n")
+
+
